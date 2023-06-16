4327361e30796d0548dfcce174b42a87f98177f6
unknown <vikriagung7@gmail.com>
2023-06-16 17:05:02 +0700

    fix: fix interface bugs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import Button from './Button';
 
 const CardComponent = ({ img, name, price, composition }) => {
   return (
-    <div className="w-[280px] h-[388px] bg-card border border-[#f0e4d7] my-8">
+    <div className="w-[280px] h-[388px] bg-card border border-[#f0e4d7] my-8 relative">
       <img
         src={`https://nwnvjsefgvlgbbdmczcm.supabase.co/storage/v1/object/public/products_img/coffee_img/${img}`}
         alt="ccoffe/img"
@@ -18,7 +18,7 @@ const CardComponent = ({ img, name, price, composition }) => {
         </p>
       </div>
 
-      <div className="flex relative top-10">
+      <div className="flex absolute left-[30%] top-[93%]">
         <div className="mx-auto">
           <Button text={'Order Now'} height={'48px'} width={'140px'} />
         </div>
