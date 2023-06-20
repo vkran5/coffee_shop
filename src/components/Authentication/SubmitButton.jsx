5daf3cfab937cfec0bbe9239b4d39022e03fe631
unknown <vikriagung7@gmail.com>
2023-06-20 15:22:04 +0700

    fix: fixing interface bugs

--- src/components/Authentication/SubmitButton.jsx
+++ src/components/Authentication/SubmitButton.jsx
@@ -2,7 +2,7 @@ const SubmitButton = () => {
   return (
     <>
       <input
-        className={`bg-yellow rounded-3xl px-5 py-3 w-[340px] md:w-[350px] h-[48px] font-poppins text-[18px] mt-5 hover:brightness-95 font-semibold`}
+        className={`bg-yellow mx-auto rounded-3xl px-5 py-3 w-[340px] md:w-[350px] h-[48px] font-poppins text-[18px] mt-5 hover:brightness-95 font-semibold`}
         type='submit'
       />
     </>
