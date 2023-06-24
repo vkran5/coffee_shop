82e5bc2cd213abb0248aa09f2c120dc6196601cf
unknown <vikriagung7@gmail.com>
2023-06-24 13:07:46 +0700

    fix: vercel deploymentd

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import supabase from 'config/supabaseClient';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { userLogin } from 'slices/userSlice';
-import AboutUs from 'Pages/AboutUs';
+import AboutUs from 'pages/AboutUs';
 import Dashboard from 'pages/Dashboard';
-import HomePage from 'Pages/HomePage';
+import HomePage from 'pages/HomePage';
 
 function App() {
   const [loading, setLoading] = useState(false);
